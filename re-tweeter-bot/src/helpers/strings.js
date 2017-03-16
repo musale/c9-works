829a9badd0cebff9b762781bb7232ced8cb613da
Martin Musale <martinmshale@gmail.com>
2017-03-16 15:03:51 +0000

    Initial Commit.

---
+++ re-tweeter-bot/src/helpers/strings.js
@@ -0,0 +1,19 @@
+module.exports = {
+  queryString: [
+    ''
+  ],
+  queryStringSubQuery: [
+    ''
+  ],
+  blockedStrings: [
+    ''
+  ],
+  resultType: [
+    'mixed',
+    'recent',
+    'popular'
+  ],
+  responseString: [
+    'Hi @screenName thanks for the following!'
+  ]
+}
