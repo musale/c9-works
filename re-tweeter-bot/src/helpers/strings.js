6fa9c38e24168b77524a7cbb77b59523ecef01fa
Martin Musale <martinmshale@gmail.com>
2017-03-17 10:57:21 +0000

    Added the strings to query.

--- re-tweeter-bot/src/helpers/strings.js
+++ re-tweeter-bot/src/helpers/strings.js
@@ -1,12 +1,33 @@
 module.exports = {
   queryString: [
-    ''
+    '100DaysOfCode',
+    'nodejs',
+    'react',
+    'reactjs',
+    'golang',
+    'android',
+    'linux',
+    'apple',
+    'elm',
+    'javascript',
+    'coding',
+    'code',
+    'programming',
+    'software engineering'
   ],
   queryStringSubQuery: [
-    ''
+    ' ',
+    ' best practises',
+    ' handytip',
+    ' latest',
+    ' updates',
+    ' environment',
+    ' news'
   ],
   blockedStrings: [
-    ''
+    'porn',
+    'sex',
+    'fuck'
   ],
   resultType: [
     'mixed',
