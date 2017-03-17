8f352c73aa1cecb6c0939aba8e980a1f536d7cf7
Martin Musale <martinmshale@gmail.com>
2017-03-17 11:05:05 +0000

    Added the environment fields.

--- re-tweeter-bot/src/config.js
+++ re-tweeter-bot/src/config.js
@@ -7,15 +7,15 @@
  * Add your account user username
  * Add your sentiment API key
  */
-require('dotenv').config()
+// require('dotenv').config()
 
 module.exports = {
   twitter: {
-    username: process.env.TWITTER_USERNAME,
-    consumerKey: process.env.TWITTER_CONSUMER_KEY,
-    consumerSecret: process.env.TWITTER_CONSUMER_SECRET,
-    accessToken: process.env.TWITTER_ACCESS_TOKEN,
-    accessTokenSecret: process.env.TWITTER_ACCESS_TOKEN_SECRET,
+    username: process.env.USERNAME,
+    consumerKey: process.env.CONSUMER_KEY,
+    consumerSecret: process.env.CONSUMER_SECRET,
+    accessToken: process.env.ACCESS_TOKEN,
+    accessTokenSecret: process.env.ACCESS_TOKEN_SECRET,
     retweet: process.env.TWITTER_RETWEET_RATE,
     favorite: process.env.TWITTER_FAVORITE_RATE
   },
